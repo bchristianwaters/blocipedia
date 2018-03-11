574898aa0084226c261fb0c1498ac8450a3189c1
EC2 Default User <ec2-user@ip-172-31-17-232.us-east-2.compute.internal>
2018-03-11 01:31:44 +0000

    Wiki UD

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -21,6 +21,31 @@
        render :new
      end 
    end
+   
+   def edit
+     @wiki = Wiki.find(params[:id])
+   end
+  
+   def update
+     @wiki = Wiki.find(params[:id])
+     @wiki.assign_attributes(wiki_params)
+ 
+     if @wiki.save
+       redirect_to @wiki
+     else
+       render :edit
+     end
+   end
+   
+   def destroy
+     @wiki = Wiki.find(params[:id])
+ 
+     if @wiki.destroy
+       redirect_to action: :index
+     else
+       render :show
+     end
+   end
     
    private
    def wiki_params
